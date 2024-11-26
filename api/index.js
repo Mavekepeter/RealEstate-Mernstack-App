a8595b73a5d7e5f4001e45916616de85e1dc4f83
samuel maveke <mavekepeter2022@gmail.com>
2024-11-26 22:22:30 +0300

    create sign up api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv';
+import authRouter from './routes/auth.route.js'
 import userRouter from './routes/user.route.js'
 
 dotenv.config();
@@ -12,10 +13,12 @@ mongoose.connect(process.env.MONGO).then(()=>{
     
 })
 const app = express();
+app.use(express.json());
 
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!!!!');
     
 })
-app.use('/api/user',userRouter)
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
